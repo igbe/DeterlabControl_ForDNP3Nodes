5462baadc56ac263f91e474c8629d486f82f03ad
obinna Igbe <obiigbe91@gmail.com>
2015-08-09 15:37:50 -0700

    modified main. added a function

--- dnpv2install/main.sh
+++ dnpv2install/main.sh
@@ -12,9 +12,14 @@ yes | git clone https://github.com/automatak/dnp3.git
 
  
 python DeterlabControl/nodecontrol/nodes.py  #to create a file /tmp/nodelist.txt that will contain list of exp. nodes
-while read line; do
-    ssh $line.Grid.TCPFlooding 'bash -s' < ~/DeterlabControl/dnpv2install/dnp3v2install1.sh
-    ssh $line.Grid.TCPFlooding 'source ~/.bashrc; echo "ASIO_HOME set to: $ASIO_HOME"'
-    ssh $line.Grid.TCPFlooding 'bash -s' < ~/DeterlabControl/dnpv2install/dnp3v2install2.sh
-done < /tmp/nodelist.txt
 
+function node_install(){
+    ssh $1.Grid.TCPFlooding 'bash -s' < ~/DeterlabControl/dnpv2install/dnp3v2install1.sh
+    ssh $1.Grid.TCPFlooding 'source ~/.bashrc; echo "ASIO_HOME set to: $ASIO_HOME"'
+    ssh $1.Grid.TCPFlooding 'bash -s' < ~/DeterlabControl/dnpv2install/dnp3v2install2.sh
+}
+
+while read line; do
+    node_install $line
+    echo "done with second"
+done < /tmp/nodelist.txt
