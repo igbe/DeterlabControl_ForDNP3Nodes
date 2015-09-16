1202a30b392d37218cc8092ceb905e94d5be97a9
obinna Igbe <obiigbe91@gmail.com>
2015-09-15 21:52:51 -0700

    changed the build tool from autoreconf to cmake

--- dnpv2install/dnp3v2install2.sh
+++ dnpv2install/dnp3v2install2.sh
@@ -4,15 +4,18 @@ cd ~/
 export ASIO_HOME=/users/oigbe000/asio/asio/include
 echo $ASIO_HOME
 
-#echo "going inside dnp3 folder..."
-cp -r dnp3 /tmp/
-#cd dnp3
-cd /tmp/dnp3
-echo "running git checkout..."
-git checkout 2.0.1
+echo "going inside dnp3 folder..."
+#cp -r dnp3 /tmp/
+cd dnp3
+#cd /tmp/dnp3
 
-echo "running autoreconf part...."
-sudo autoreconf -i -f && ./configure && make && sudo make install
+echo "running git checkout..."
+#git checkout 2.0.1
+git checkout --detach 2.0.x
+echo "running cmake..."
+#echo "running autoreconf part...."
+
+/opt/cmake/bin/cmake -DCMAKE_INSTALL_PREFIX=/usr -DDEMO=ON && make -j && sudo make install
 
 echo "done with installation"
 
