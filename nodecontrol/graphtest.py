a572114003182408c6233f5a1794c262400bcbaf
Obinna Igbe <obiigbe91@gmail.com>
2015-09-22 22:39:04 -0400

    modified graphtest.py

--- nodecontrol/graphtest.py
+++ nodecontrol/graphtest.py
@@ -17,42 +17,40 @@ def sshwithcmd(node,command):
         return output,err
 
 def task(host,time):
-	command1="ip route get 10.1.1.1 | cut -b 14-17; rm ~/DeterlabControl/nodecontrol/*data.txt"
-	output1,err=sshwithcmd(host,command1)
-	#print err
-	i=0
-	prev=[0,0]
-	finaldata=[]
-	while i < time:
-		command="cat /sys/class/net/%s/statistics/tx_bytes; cat /sys/class/net/%s/statistics/rx_bytes; sleep 1; cat /sys/class/net/%s/statistics/tx_bytes; cat /sys/class/net/%s/statistics/rx_bytes"%(output1[0:4],output1[0:4],output1[0:4],output1[0:4])
-		x=tm.ctime()
-		output,err=sshwithcmd(host,command)
-		#t1,r1,t2,r2,n=output.split("\n")
-		
-		val=output.split("\n")
-		#print val
-		#print prev
-		#print str(int(val[2])-int(val[0]))+","+str(int(val[3])-int(val[1]))
+        command1="ip route get 10.1.1.1 | cut -b 14-17; rm ~/DeterlabControl/nodecontrol/*data.txt"
+        output1,err=sshwithcmd(host,command1)
+        #print err
+        i=0
+        prev=[0,0]
+        #finaldata=[]
+        test=""
+        while i < time:
+                command="cat /sys/class/net/%s/statistics/tx_bytes; cat /sys/class/net/%s/statistics/rx_bytes; sleep 1; cat /sys/class/net/%s/statisti$
+                x=tm.ctime()
+                output,err=sshwithcmd(host,command)
+                #t1,r1,t2,r2,n=output.split("\n")
 
-		if i>=1:
-			data=str(int(val[0])-int(prev[0]))+","+str(int(val[1])-int(prev[1]))+","+str(x)+"\n"  #print str(int(val[0])-int(prev[0]))+","+str(int(val[1])-int(prev[1]))+","+str(x)
+                val=output.split("\n")
+                #print val
+                #print prev
+                #print str(int(val[2])-int(val[0]))+","+str(int(val[3])-int(val[1]))
+                if i>=1:
+                        data=str(int(val[0])-int(prev[0]))+","+str(int(val[1])-int(prev[1]))+","+str(x)+"\n"  #print str(int(val[0])-int(prev[0]))+","$
 
-			#myfile1 = open('%sdata.txt'%(host), 'a')
-		        #myfile1.write(data)
-		        #myfile1.close()			
-			#print "closing file"
-			finaldata.append(data)
-			prev[0]=val[2]
-			prev[1]=val[3]
-		else:
-			prev[0]=val[2]
+                        #finaldata.append(data)
+                        test=test+data
+                        prev[0]=val[2]
                         prev[1]=val[3]
-	
-		
-		i=i+1
-	print finaldata
+                else:
+                        prev[0]=val[2]
+                        prev[1]=val[3]
 
-	
+
+                i=i+1
+        #print finaldata
+        print test
+        print host.split(".")[0]
+
 def main():
 
         parser = argparse.ArgumentParser(description="Plot the graph of a DeterLab node characteristics againsts time")
@@ -66,3 +64,7 @@ def main():
         task(host,time)
 if __name__ == "__main__":
         main()
+
+
+
+
