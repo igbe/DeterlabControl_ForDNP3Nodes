16a3bd32a69346ad66f845de65d5a7f1e96c3329
obinna Igbe <obiigbe91@gmail.com>
2015-12-26 04:24:44 +0000

    update to the cmake installation

--- SA-DNP3/cmake-3.4.1-installation.sh
+++ SA-DNP3/cmake-3.4.1-installation.sh
@@ -1,6 +1,6 @@
-wget https://cmake.org/files/v3.4/cmake-3.4.1.tar.gz
-tar xzf cmake-3.4.1.tar.gz
-cd cmake-3.4.1
+wget https://cmake.org/files/v3.2/cmake-3.2.3.tar.gz
+tar xzf cmake-3.2.3.tar.gz
+cd cmake-3.2.3
 ./configure --prefix=/opt/cmake
 make
 make install
@@ -9,4 +9,4 @@ make install
 /opt/cmake/bin/cmake -version
 
 #The output should look something like below (depending upon cmake version you are installing).
-#cmake version 3.4.1
+#cmake version 3.2.3
